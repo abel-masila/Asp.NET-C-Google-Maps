06b37cf5701dd0fbd5255c4e504c082b796793a4
Abel Masila <abelmasila@gmail.com>
2016-06-15 16:59:08 +0300

    First Commit

---
+++ GoogleMaps/Controllers/MapController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GoogleMaps.Models;
+
+namespace GoogleMaps.Controllers
+{
+    public class MapController : Controller
+    {
+        //
+        // GET: /Map/
+
+        public ActionResult Index()
+        {
+            MapsEntities GE = new MapsEntities();
+            return View(GE.Sheet1_.ToList().Take(50));
+        }
+        [HttpPost]
+        public ActionResult Search(string Location)
+        {
+
+          MapsEntities GE = new MapsEntities();
+            var result = GE.Sheet1_.Where(x => x.Name.StartsWith(Location)).Take(50).ToList();
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
